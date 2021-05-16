dd0796edc00abb7a562c829fd19f775a27dec48c
Augusto Araújo <augustoaraujo4520@gmail.com>
2021-05-16 17:41:50 -0300

    Classe conexao e classe conexaoException 1.0

---
+++ src/Conexao/ConexaoException.java
@@ -0,0 +1,10 @@
+package Conexao;
+
+public class ConexaoException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ConexaoException(String msg) {
+        super(msg);
+    }
+}
