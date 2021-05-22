77ac7417858c4bd76b29d60ff240eb2d78e282d8
Augusto Araújo <augustoaraujo4520@gmail.com>
2021-05-21 23:33:50 -0300

    TelaPrincipal verão 1.4 | Hora e data atual na barra de saudações- 1.5

--- src/Telas/TelaPrincipal.java
+++ src/Telas/TelaPrincipal.java
@@ -2,26 +2,66 @@ package Telas;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.EventQueue;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 import javax.swing.MenuElement;
+import javax.swing.Timer;
 import javax.swing.plaf.basic.BasicMenuBarUI;
 
 public class TelaPrincipal extends javax.swing.JFrame {
 
     public TelaPrincipal() {
         initComponents();
+
+        // para chamar a hora HH:mm:ss
+        // Esse codigo exibe a data do sistema
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        Date data = new Date();
+        String atual = df.format(data);
+        LblDataAtual.setText(atual);
+
+        horaAtual.start();
     }
+    
+    JLabel lblHora = new JLabel();
+    
+    Thread horaAtual = new Thread(new Runnable() { //cria uma thread
+        public void run() {
+            while(true) { //roda indefinidamente
+                Date data = Calendar.getInstance().getTime();
+                DateFormat d = DateFormat.getDateInstance();
+                DateFormat h = DateFormat.getTimeInstance();
+                LblHoraAtual.setText(h.format(data));     
+                try {
+                    Thread.sleep(1000); //espera 1 segundo para fazer a nova evolução
+                } catch(InterruptedException ex){
+                    System.out.println("Erro ao buscra hora atual");
+                }
+            }
+        }
+    }); 
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
+        PainelPrincipal = new javax.swing.JPanel();
+        PainelDeSaudacao = new javax.swing.JPanel();
+        LblData = new javax.swing.JLabel();
+        LblDataAtual = new javax.swing.JLabel();
+        LblHora = new javax.swing.JLabel();
+        LblHoraAtual = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         MenuCadastro = new javax.swing.JMenu();
         MenuUsuario = new javax.swing.JMenuItem();
@@ -32,18 +72,65 @@ public class TelaPrincipal extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(220, 199, 170));
-        jPanel1.setToolTipText("Tela principal");
+        PainelPrincipal.setBackground(new java.awt.Color(220, 199, 170));
+        PainelPrincipal.setToolTipText("Tela principal");
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 745, Short.MAX_VALUE)
+        PainelDeSaudacao.setBackground(new java.awt.Color(40, 109, 168));
+
+        LblData.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        LblData.setForeground(new java.awt.Color(255, 255, 255));
+        LblData.setText("Data:");
+
+        LblDataAtual.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        LblDataAtual.setForeground(new java.awt.Color(255, 255, 255));
+        LblDataAtual.setText("------");
+
+        LblHora.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        LblHora.setForeground(new java.awt.Color(255, 255, 255));
+        LblHora.setText("Hora:");
+
+        LblHoraAtual.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        LblHoraAtual.setForeground(new java.awt.Color(255, 255, 255));
+        LblHoraAtual.setText("------");
+
+        javax.swing.GroupLayout PainelDeSaudacaoLayout = new javax.swing.GroupLayout(PainelDeSaudacao);
+        PainelDeSaudacao.setLayout(PainelDeSaudacaoLayout);
+        PainelDeSaudacaoLayout.setHorizontalGroup(
+            PainelDeSaudacaoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(PainelDeSaudacaoLayout.createSequentialGroup()
+                .addGap(14, 14, 14)
+                .addComponent(LblData)
+                .addGap(18, 18, 18)
+                .addComponent(LblDataAtual, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(LblHora)
+                .addGap(18, 18, 18)
+                .addComponent(LblHoraAtual, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(463, Short.MAX_VALUE))
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 477, Short.MAX_VALUE)
+        PainelDeSaudacaoLayout.setVerticalGroup(
+            PainelDeSaudacaoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, PainelDeSaudacaoLayout.createSequentialGroup()
+                .addContainerGap(20, Short.MAX_VALUE)
+                .addGroup(PainelDeSaudacaoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(LblData)
+                    .addComponent(LblDataAtual)
+                    .addComponent(LblHora)
+                    .addComponent(LblHoraAtual))
+                .addContainerGap())
+        );
+
+        javax.swing.GroupLayout PainelPrincipalLayout = new javax.swing.GroupLayout(PainelPrincipal);
+        PainelPrincipal.setLayout(PainelPrincipalLayout);
+        PainelPrincipalLayout.setHorizontalGroup(
+            PainelPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(PainelDeSaudacao, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        PainelPrincipalLayout.setVerticalGroup(
+            PainelPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(PainelPrincipalLayout.createSequentialGroup()
+                .addComponent(PainelDeSaudacao, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 432, Short.MAX_VALUE))
         );
 
         MenuCadastro.setText("Cadastro");
@@ -75,11 +162,11 @@ public class TelaPrincipal extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(PainelPrincipal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(PainelPrincipal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         setSize(new java.awt.Dimension(761, 539));
@@ -91,10 +178,10 @@ public class TelaPrincipal extends javax.swing.JFrame {
         menuBar.setUI(new BasicMenuBarUI() {
 
             public void paint(Graphics g, JComponent c) {
-                  //[220,199,170]
-                  //[107,122,143]
-                  //[6,47,79]
-                g.setColor(new java.awt.Color(6,47,79));
+                //[220,199,170]
+                //[107,122,143]
+                //[6,47,79]
+                g.setColor(new java.awt.Color(6, 47, 79));
                 g.fillRect(0, 0, c.getWidth(), c.getHeight());
             }
 
@@ -122,18 +209,17 @@ public class TelaPrincipal extends javax.swing.JFrame {
                     JMenuItem menuItem = (JMenuItem) menuItemElement.getComponent();
                     changeComponentColors(menuItem);
                     menuItem.setOpaque(true);
-                    
+
                 }
             }
         }
     }
 
     private void changeComponentColors(Component comp) {
-        comp.setBackground(new java.awt.Color(247,195,49));
+        comp.setBackground(new java.awt.Color(247, 195, 49));
         comp.setForeground(Color.WHITE);
     }
 
-    
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -158,7 +244,6 @@ public class TelaPrincipal extends javax.swing.JFrame {
         }
         //</editor-fold>
 
-  
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new TelaPrincipal().setVisible(true);
@@ -167,13 +252,18 @@ public class TelaPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    javax.swing.JLabel LblData;
+    javax.swing.JLabel LblDataAtual;
+    javax.swing.JLabel LblHora;
+    javax.swing.JLabel LblHoraAtual;
     javax.swing.JMenuItem MenuAluno;
     javax.swing.JMenu MenuCadastro;
     javax.swing.JMenuItem MenuUsuario;
+    javax.swing.JPanel PainelDeSaudacao;
+    javax.swing.JPanel PainelPrincipal;
     javax.swing.JMenu jMenu1;
     javax.swing.JMenu jMenu2;
     javax.swing.JMenu jMenu3;
     javax.swing.JMenuBar jMenuBar1;
-    javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
